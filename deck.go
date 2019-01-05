a3639cfd5a6e8a1220771f92d69614667282c0f6
lukewalt <lukewdwrd.dev@gmail.com>
2019-01-05 12:19:53 -0600

    created newDeck function that returns a new deck type

--- deck.go
+++ deck.go
@@ -2,14 +2,29 @@ package main
 
 import "fmt"
 
-// tells Go we want to create array of strings as well as functions specific to it
-// func w/ reciever is like a method to an instance
+// tells Go we want to create a new type
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, val := range cardValues {
+			cards = append(cards, val+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+// reciever is like a method to an instance spec to type
 // (input type) any variable of type deck gets access to this method
 // uses one/two letters from type instead of 'self' or 'this'
 func (d deck) print() {
-	for i, card := range d {
-		fmt.Println(i, card)
+	for _, card := range d {
+		fmt.Println(card)
 	}
 }
