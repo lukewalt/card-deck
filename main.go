7b5126f048e0575ebe72f02568930ad454247726
lukewalt <lukewdwrd.dev@gmail.com>
2019-01-05 12:20:24 -0600

    moved deck function to deck.go

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 func main() {
-	cards := deck{newCard(), newCard()}
+	cards := newDeck()
 	cards = append(cards, "Six of Spades") // immutable function
 
 	// pass var cards to print
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
