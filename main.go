f86db6ca1d5118758f23851bd28695be5014ff10
lukewalt <lukewdwrd.dev@gmail.com>
2019-01-05 21:52:21 -0600

    instanciated a new deck of cards, shuffled, and dealt them

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
 	// assigns a new deck to a cards var
 	cards := newDeck()
+	cards.shuffle()
 
 	// two vars assigned to respective return values from deal()
 	hand, remainingCards := deal(cards, 5)
 
+	fmt.Println("-- Hand --")
 	hand.print()
+
+	fmt.Println("-- Remaining Cards --")
 	remainingCards.print()
 }
